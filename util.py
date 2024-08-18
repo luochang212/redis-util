95b561d4d4c48d2bc14b2922fee587883ac08908
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-08-19 02:32:20 +0800

    update

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import json
 import redis
+import redisearch
 
 
 class RedisHandler:
@@ -36,6 +37,66 @@ class RedisHandler:
 
     def delete(self, key):
         return self.rc.delete(key)
+
+
+class RediSearchHandler:
+
+    def __init__(self,
+                 name: str,
+                 host: str = 'localhost',
+                 port: int = 6379,
+                 password: str = None):
+        if password is None:
+            self.rc = redisearch.Client(name, host=host, port=port)
+        else:
+            self.rc = redisearch.Client(name, password=password, host=host, port=port)
+
+    def ping(self):
+        self.rc.redis.ping()
+
+    def set(self, key, value):
+        return self.rc.redis.set(key, value)
+
+    def get(self, key):
+        return self.rc.redis.get(key)
+
+
+class RedisIndexHandler:
+    
+    def __init__(self,
+                 host: str = 'localhost',
+                 port: int = 6379,
+                 password: str = None):
+        if password is None:
+            self.rc = redis.Redis(host=host, port=port)
+        else:
+            self.rc = redis.Redis(host=host, port=port, password=password)
+
+    def ping(self):
+        self.rc.ping()
+
+    def create_index(self, cmd):
+        return self.rc.execute_command(*cmd)
+
+    def delete_index(self, index_name):
+        return self.rc.execute_command(f'FT.DROPINDEX {index_name} [DD]')
+
+    def insert_index(self, key: str, data: dict):
+        return self.rc.hset(key, mapping=data)
+
+    def get_data(self, key: str):
+        return self.rc.execute_command('HGETALL', key)
+
+    def del_data(self, key: str):
+        return self.rc.delete(key)
+
+    def search_vector(self, query_vec, index_name, vector_field_name, num_return_vec):
+        query_vec_binary = query_vec.tobytes()
+        cmd = [
+            'FT.SEARCH', index_name, f'*=>[KNN {num_return_vec} @{vector_field_name} $query_vec]', 
+            'PARAMS', '2', 'query_vec', query_vec_binary, 'DIALECT', '2'
+        ]
+        return self.rc.execute_command(*cmd)
 
 
 class Cast:
