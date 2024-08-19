eab06989927bf264929d7e8e2891ecf730460ac2
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-08-19 22:44:48 +0800

    update

--- merge.sh
+++ merge.sh
@@ -1,3 +1,3 @@
 output_ipynb='all.ipynb'
 nbmerge 1.安装Redis.ipynb 2.用Redis存取Embedding.ipynb 3.RediSearch模块.ipynb \
-    4.开源向量引擎Milvus.ipynb 5.RediSearch自制强化版.ipynb  > ${output_ipynb}
+    4.开源向量引擎Milvus.ipynb 5.自制强化版向量引擎.ipynb  > ${output_ipynb}
